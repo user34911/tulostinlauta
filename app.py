970e4bbf0b8f0bff85cedd834298b7bfd2f59848
Kristian Tiilola <180793269+user34911@users.noreply.github.com>
2025-03-28 14:17:20 +0200

    Mahdollistettu julkaisujen tekeminen

--- app.py
+++ app.py
@@ -13,6 +13,24 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_post")
+def new_post():
+    return render_template("new_post.html")
+
+@app.route("/create_post", methods=["POST"])
+def create_post():
+    title = request.form["title"]
+    model_year = request.form["model_year"]
+    grade = request.form["grade"]
+    review = request.form["review"]
+    user_id = session["user_id"]
+
+    sql = """INSERT INTO posts (title, model_year, grade, review, user_id)
+    VALUES (?, ?, ?, ?, ?)"""
+    db.execute(sql, [title, model_year, grade, review, user_id])
+
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -43,10 +61,14 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
+
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
@@ -54,5 +76,6 @@ def login():
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,12 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    model_year INTEGER,
+    grade INTEGER,
+    review TEXT,
+    user_id INTEGER REFERENCES users
+);
