361be11e4f87d83acc835e5eb75a461d8b397d00
Kristian Tiilola <180793269+user34911@users.noreply.github.com>
2025-04-09 13:16:13 +0300

    Refactor registration/login

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import sqlite3
 from flask import Flask
 from flask import redirect, render_template, request, session, abort
-from werkzeug.security import generate_password_hash, check_password_hash
 import db
 import config
 import posts, users
@@ -122,11 +121,9 @@ def create():
     password2 = request.form["password2"]
     if password1 != password2:
         return "VIRHE: salasanat eivät ole samat"
-    password_hash = generate_password_hash(password1)
 
     try:
-        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-        db.execute(sql, [username, password_hash])
+        users.create_user(username, password1)
     except sqlite3.IntegrityError:
         return "VIRHE: tunnus on jo varattu"
 
@@ -141,13 +138,9 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        sql = "SELECT id, password_hash FROM users WHERE username = ?"
-        result = db.query(sql, [username])[0]
-        user_id = result["id"]
-        password_hash = result["password_hash"]
+        user_id = users.check_login(username, password)
 
-
-        if check_password_hash(password_hash, password):
+        if user_id:
             session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
